9e8c53e8c63291cc745fe0a57fc1af2537ef5d54
jpgb-hub <jpgbonnet@gmail.com>
2023-06-25 20:42:41 -0400

    guarda

--- src/app.js
+++ src/app.js
@@ -2,10 +2,62 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suits = ["♠", "♣", "♥", "♦"];
+
+  function getRandomCard() {
+    const randomNumber = Math.floor(Math.random() * numbers.length);
+    const randomSuit = Math.floor(Math.random() * suits.length);
+    const suit = suits[randomSuit];
+
+    return {
+      suit: suit,
+      number: numbers[randomNumber]
+    };
+  }
+
+  function displayRandomCard() {
+    const card = getRandomCard();
+    const cardContainer = document.getElementById("cardContainer");
+
+    // Clear previous card
+    cardContainer.innerHTML = "";
+
+    const suitElement = document.createElement("div");
+    suitElement.className = "suit";
+    suitElement.textContent = card.suit;
+
+    const numberElement = document.createElement("div");
+    numberElement.className = "number";
+    numberElement.textContent = card.number;
+
+    cardContainer.appendChild(suitElement);
+    cardContainer.appendChild(numberElement);
+  }
+
+  // Display a random card initially
+  displayRandomCard();
+
+  // Generate a new random card on button click
+  const button = document.createElement("button");
+  button.textContent = "Get a Random Card";
+  button.addEventListener("click", displayRandomCard);
+  document.body.appendChild(button);
 };
